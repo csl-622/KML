2d5538b1551dc59741fa469654d10849c642d9b9
paras_kumar <2016csb1047@iitrpr.ac.in>
2018-10-19 06:29:42 +0530

    Added decoder.py
    This program is used to get back the full text of edited article from revisions.

---
+++ decoder.py
@@ -0,0 +1,25 @@
+f = open("article.txt")
+s = f.read().split()
+
+f2 = open("revision")
+tmp = f2.read().split("\n")
+
+n = int(input(str(len(tmp)-1)+" Revisons found, enter the revision number to be loaded: "))
+
+
+s2 = tmp[n-1].split()
+
+index = 0
+
+for x in s2:
+	if x.isdigit():
+		for i in range(index, index+int(x)):
+			print(s[i], end=" ")
+			index += 1
+	else:
+		if x[0] == '-':
+			for i in range(index, index+int(x[1:])):
+				index += 1
+		else:
+			print(x, end=" ")		
+print("")				
