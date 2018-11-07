1ddd753edc7af1d22aba4026ffbaf0f5c593a7df
paras_kumar <2016csb1047@iitrpr.ac.in>
2018-11-07 21:55:48 +0530

    Now supports KML

--- decoder.py
+++ decoder.py
@@ -1,12 +1,13 @@
 import xml.etree.ElementTree as ET
+import xml.dom.minidom
 
-ET._namespace_map['http://www.mediawiki.org/xml/export-0.10/'] = ''
-ET._namespace_map['http://www.w3.org/2001/XMLSchema-instance'] = 'xsi'
-tree = ET.parse('latest.xml')
+#ET._namespace_map['http://www.mediawiki.org/xml/export-0.10/'] = ''
+#ET._namespace_map['http://www.w3.org/2001/XMLSchema-instance'] = 'xsi'
+tree = ET.parse('latest.kml')
 root = tree.getroot()
 
-rev = root[1].find('{http://www.mediawiki.org/xml/export-0.10/}revision')
-root[1].remove(rev)
+rev = root[0].find('Instance')
+root[0].remove(rev)
 str1 = ET.tostring(rev).decode("utf-8")
 s = str1.split(" ")
 
@@ -45,7 +46,7 @@ for x in s2:
 			index += 1
 	elif x[0] == "'" and x[-1] == "'" and x[1:-1].isdigit():
 #			print(x[1:-1].replace("`", "\n").replace("~", "-"), end=" ")				 
-			result += x[1:-1].replace("`", "\n").replace("~", "-")
+			result += x[1:-1].replace("`", "\n			").replace("~", "-")
 			result += " "
 	else:
 		if x[0] == '-':
@@ -53,9 +54,17 @@ for x in s2:
 				index += 1
 		else:
 #			print(x.replace("`", "\n").replace("~", "-"), end=" ")
-			result += x.replace("`", "\n").replace("~", "-")		
+			result += x.replace("`", "\n			").replace("~", "-")		
 			result += " "
 
-root[1].append(ET.fromstring(result))
-tree.write('output.xml')
-print(n, "th revision written to output.xml")
+root[0].append(ET.fromstring(result))
+tree.write('output.kml')
+print(n, "th revision written to output.kml")
+
+f = open('output.kml')
+f_str = f.read()
+f.close()
+
+f2 = open('output.kml', "w")
+f2.write("<?xml version='1.0' encoding='utf-8'?>\n"+f_str)
+f2.close()
